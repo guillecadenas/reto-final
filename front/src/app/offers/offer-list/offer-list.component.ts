5bdeff82143185287efb48e40d59a468c0859a2e
Pedro Núñez <pedro@eb-creativos.com>
2020-03-12 11:26:16 +0100

    Listado de ofertas, version preliminar (botones Edit y Delete no son funcionales)

--- front/src/app/offers/offer-list/offer-list.component.ts
+++ front/src/app/offers/offer-list/offer-list.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+/**
+ * @author Pedro Núñez
+ */
+
+import {Component, OnInit} from '@angular/core';
+import {OfferService} from '../offer.service';
+import {Offer} from '../offer';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-offer-list',
@@ -6,10 +13,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./offer-list.component.css']
 })
 export class OfferListComponent implements OnInit {
+  errorMessage: string;
+  offers: Offer[];
 
-  constructor() { }
+  constructor(private router: Router, private offerService: OfferService) {
+  }
 
   ngOnInit() {
+    this.offerService.getOffers().subscribe(
+      offers => this.offers = offers,
+      error => this.errorMessage = error as any);
+  }
+
+  onSelect(offer: Offer) {
+    this.router.navigate(['/offers', offer.id]);
+  }
+
+  addOffer() {
+    this.router.navigate(['/offers/add']);
   }
 
 }
