53cd29c1f9f05a662282095dc310b78d4fc83838
Pedro Núñez <pedro@eb-creativos.com>
2020-03-12 10:20:56 +0100

    Modulo routing + configuracion en offers.module

---
+++ front/src/app/offers/offers-routing.module.ts
@@ -0,0 +1,25 @@
+/**
+ * @author Pedro Núñez
+ */
+
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {OfferValidComponent} from './offer-valid/offer-valid.component';
+import {OfferListComponent} from './offer-list/offer-list.component';
+import {OfferEditComponent} from './offer-edit/offer-edit.component';
+import {OfferAddComponent} from './offer-add/offer-add.component';
+
+const ownerRoutes: Routes = [
+  {path: 'offers', component: OfferValidComponent},
+  {path: 'offers/add', component: OfferAddComponent},
+  {path: 'offers/list', component: OfferListComponent},
+  {path: 'offers/:id/edit', component: OfferEditComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(ownerRoutes)],
+  exports: [RouterModule]
+})
+
+export class OffersRoutingModule {
+}
--- front/src/app/offers/offers.module.ts
+++ front/src/app/offers/offers.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { OfferAddComponent } from './offer-add/offer-add.component';
 import { OfferListComponent } from './offer-list/offer-list.component';
 import { OfferEditComponent } from './offer-edit/offer-edit.component';
 import { OfferValidComponent } from './offer-valid/offer-valid.component';
+import { OffersRoutingModule } from './offers-routing.module';
+
 
 @NgModule({
   declarations: [OfferAddComponent, OfferListComponent, OfferEditComponent, OfferValidComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    OffersRoutingModule
   ]
 })
 export class OffersModule { }
