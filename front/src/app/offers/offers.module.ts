1b85fea82cefe85aebb523931878d4132c9a61cf
Alberto <alberamon86@gmail.com>
2020-03-12 10:11:57 +0100

    creación esqueleto

--- front/src/app/offers/offers.module.ts
+++ front/src/app/offers/offers.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OfferComponent } from './offer/offer.component';
+import { OfferAddComponent } from './offer-add/offer-add.component';
+import { OfferListComponent } from './offer-list/offer-list.component';
+import { OfferEditComponent } from './offer-edit/offer-edit.component';
+import { OfferValidComponent } from './offer-valid/offer-valid.component';
 
 @NgModule({
-  declarations: [OfferComponent],
+  declarations: [OfferAddComponent, OfferListComponent, OfferEditComponent, OfferValidComponent],
   imports: [
     CommonModule
   ]
