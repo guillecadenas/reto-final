c34dd3e23dba9852ae6bbad16350c1aa30db8bdd
Manuel <lolo-lozano1498@hotmail.com>
2020-03-12 11:09:08 +0100

    Servicio modificado.

--- front/src/app/offers/offer.service.ts
+++ front/src/app/offers/offer.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import { Offer } from './offer';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OfferService {
 
-  constructor() { }
+  entityUrl = environment.REST_API_URL + 'offers/'; //  http://localhost:9966/petclinic/api/offers
+
+  constructor(private httpClient: HttpClient) { }
+
+  getOffers(){
+    return this.httpClient.get<Offer[]>(this.entityUrl);
+  }
+  setOffers(offer: Offer){
+    return this.httpClient.post<Offer>(this.entityUrl, offer);
+  }
+  getOfferById(id: number){
+    return this.httpClient.get<Offer>(`${this.entityUrl}/${id}`);
+  }
+
+  deleteOffer(id: number){
+    return this.httpClient.delete<Offer>(`${this.entityUrl}/${id}`);
+  }
+  updateOffer(offer: Offer){
+    return this.httpClient.put<Offer>(this.entityUrl, offer);
+  }
+
+
+  
 }
